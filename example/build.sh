9c2d86ad8ccbf200107a96c009c3d678a9f941f5
jtain <jtain@debian>
2023-04-03 23:18:04 -0400

    modified build.sh to account for parser_utils

--- example/build.sh
+++ example/build.sh
@@ -1,3 +1,3 @@
-gcc -o synth synth_example.c ../track_parser.c ../vecimg.c ../synth.c ../freq.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+gcc -o synth synth_example.c ../track_parser.c ../vecimg.c ../synth.c ../freq.c ../parser_utils.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
 
-gcc -o editor vecimg_editor_example.c ../track_parser.c ../vecimg.c ../vecimg_editor.c ../synth.c ../freq.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+gcc -o editor vecimg_editor_example.c ../track_parser.c ../vecimg.c ../vecimg_editor.c ../synth.c ../freq.c ../parser_utils.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
